f96fa54a5e56782fa65edbca68cac61053549d19
Patrick Maweu <muilipato@gmail.com>
2023-04-08 00:24:55 +0300

    made nairobi weather default landing

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ const getWeather = (city)=>{
 	  .then(response => response.json())
   
 	  .then(data => {
-      console.log(data);
-      document.getElementById("city-name").innerHTML=`${data.location.name}`;
-      document.getElementById("temp").innerHTML = `${data.current.temp_c} °C`;
-      document.getElementById("clouds").innerHTML=data.current.condition.text;
-      document.querySelector(".cloud-icon").src =data.current.condition.icon;
-      document.getElementById("wind").innerHTML =`Wind Speed: ${data.current.wind_kph} km/h`;
-      precip
-      pressure
+      console.log(data); 
+      document.getElementById("nbo").innerHTML=`${data.location.name}`;
+      document.getElementById("nbo-temp").innerHTML = `${data.current.temp_c} °C`;
+      document.getElementById("nbo-clouds").innerHTML=data.current.condition.text;
+      document.querySelector(".nbo-cloud-icon").src =data.current.condition.icon;
+      document.getElementById("nbo-wind").innerHTML =`Wind Speed: ${data.current.wind_kph} km/h`;
+      
+      
 
     })
     
@@ -28,10 +28,38 @@ const getWeather = (city)=>{
 
  
 }
+function displayWeather(){
+  getWeather('Nairobi')
+}
+displayWeather();
+const anyCity = (city)=>{
+
+  fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`, options)
+	  .then(response => response.json())
+  
+	  .then(data => {
+      console.log(data);
+      document.getElementById("city-name").innerHTML=`${data.location.name}`;
+      document.getElementById("temp").innerHTML = `${data.current.temp_c} °C`;
+      document.getElementById("clouds").innerHTML=data.current.condition.text;
+      document.querySelector(".cloud-icon").src =data.current.condition.icon;
+      document.getElementById("wind").innerHTML =`Wind Speed: ${data.current.wind_kph} km/h`;
+      
+      
+
+    })
+    
+	  .catch(err => console.error(err));
+   
+
+ 
+}
+
+
 document.getElementById("submit-search").addEventListener("click", function (){
   let searchingFor=document.getElementById("weather-search").value;
-  getWeather(searchingFor);
-})
+  anyCity(searchingFor);
+}) 
 
  
 
